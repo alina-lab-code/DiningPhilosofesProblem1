19d6249d6ef47b2c7554792f08670b81c4694d9f
Owner <michalalhazov12@gmail.com>
2026-05-26 16:18:55 +0300

    add scene

--- .idea/src/MainScene.java
+++ .idea/src/MainScene.java
@@ -4,5 +4,31 @@ public class MainScene extends JPanel {
     public MainScene(int x, int y, int width, int height) {
         this.setBounds(x, y, width, height);
         this.setFocusable(true);
+
+        private final int NUM_PHILOSOPHERS = 5;
+        private final Philosopher[] philosophers = new Philosopher[NUM_PHILOSOPHERS];
+        private final Lock[] forks = new ReentrantLock[NUM_PHILOSOPHERS];
+
+
+        private final String[] states = new String[NUM_PHILOSOPHERS];
+        private final Color[] philosopherColors = new Color[NUM_PHILOSOPHERS];
+        private final Color[] forkColors = new Color[NUM_PHILOSOPHERS];
+        private final int[] eatCounters = new int[NUM_PHILOSOPHERS];
+
+    public MainScene() {
+            setBackground(Color.WHITE);
+
+            for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+                forks[i] = new ReentrantLock();
+                forkColors[i] = Color.BLACK; // Свободная вилка — черная
+            }
+
+            // Создаем и запускаем потоки философов
+            for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+                philosophers[i] = new Philosopher(i);
+                new Thread(philosophers[i]).start();
+            }
+        }
     }
 }
+
