926207df8a470444c2884d034ad0805901e46ef5
alina-lab-code <alinhoob@edu.aac.ac.il>
2026-05-26 16:09:03 +0300

    added window

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,3 +1,20 @@
+import javax.swing.*;
+
 public class Main {
-    String str = " hiiii";
-}
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Dining Philosophers Problem");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(WIDTH, HEIGHT);
+
+            MainScene panel = new MainScene();
+            frame.add(panel);
+            frame.setVisible(true);
+        });
+
+    }
+
+    }
+
--- .idea/src/MainScene.java
+++ .idea/src/MainScene.java
@@ -1,2 +1,8 @@
-public class MainScene {
+import javax.swing.*;
+
+public class MainScene extends JPanel {
+    public MainScene(int x, int y, int width, int height) {
+        this.setBounds(x, y, width, height);
+        this.setFocusable(true);
+    }
 }
