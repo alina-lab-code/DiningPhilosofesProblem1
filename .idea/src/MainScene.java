3c5b235b1036932b89be34ea60bedfc0f4c09ea2
alina-lab-code <alinhoob@edu.aac.ac.il>
2026-05-26 16:28:04 +0300

    test2

--- .idea/src/MainScene.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class MainScene extends JPanel {
-    public MainScene(int x, int y, int width, int height) {
-        this.setBounds(x, y, width, height);
-        this.setFocusable(true);
-    }
-}
